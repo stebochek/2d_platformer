8a00201ce63f54714c7e22ad64621b18615c4433
stebochek <stebochek@yandex.ru>
2023-06-10 18:19:35 +0300

    init

---
+++ settings.py
@@ -0,0 +1,17 @@
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    '                            ',
+    '                            ',
+    '                            ',
+    '                            ',
+    '        X                   ',
+    '       XXXXX                ',
+    '    P                       ',
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+
---
+++ support.py
@@ -0,0 +1,14 @@
+from os import walk
+import pygame
+
+
+def import_folder(path):
+    surface_list = []
+
+    for _, __, image_files in walk(path):
+        for image in image_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+
+    return surface_list
