45a44ff1b496a1b95f06890fe50bf9a05f8c0635
AlekseyDemkin <113420268+AlekseyDemkin@users.noreply.github.com>
2023-06-12 01:04:54 +0300

    Updated setting.py for new level setup
    В связи использоваиям Tiled, была убрана предыдущая система построения уровня.

--- settings.py
+++ settings.py
@@ -1,17 +1,5 @@
-level_map = [
-    '                            ',
-    '                            ',
-    '                            ',
-    '                            ',
-    '                            ',
-    '                            ',
-    '                            ',
-    '        X                   ',
-    '       XXXXX                ',
-    '    P                       ',
-    'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+vertical_tile_number = 22
+tile_size = 32
 
-tile_size = 64
+screen_height = vertical_tile_number * tile_size
 screen_width = 1200
-screen_height = len(level_map) * tile_size
-
