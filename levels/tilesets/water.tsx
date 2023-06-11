a5c1aac39508acd13231bebad59fbb3469c1213d
stebochek <stebochek@yandex.ru>
2023-06-12 01:22:00 +0300

    Changed and added some graphics

---
+++ levels/tilesets/water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="water" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="../../graphics/water/water.png" width="96" height="32"/>
+</tileset>
