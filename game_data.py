70af483db2740ca11693cc53f42adf8b9fd6d826
AlekseyDemkin <113420268+AlekseyDemkin@users.noreply.github.com>
2023-06-12 01:14:07 +0300

    Added game_data.py and enemy.py
    game_data пусть к ассетам
    enemy.py логика куриц

---
+++ game_data.py
@@ -0,0 +1,11 @@
+level1 = {
+    'terrain': './levels/0/0_terrain.csv',
+    'coins': './levels/0/0_coins.csv',
+    'trees': './levels/0/0_trees.csv',
+    'fence': './levels/0/0_fence.csv',
+    'building0': './levels/0/0_building0.csv',
+    'building1': './levels/0/0_building1.csv',
+    'enemies': './levels/0/0_enemies.csv',
+    'constraints': './levels/0/0_constraints.csv',
+    'player': './levels/0/0_player.csv',
+    'grass': './levels/0/0_grass.csv'}
