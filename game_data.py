52567ced946667e87b5b81d5e380bcab84b67a2f
zelyfcggff <113138046+zelyfcggff@users.noreply.github.com>
2023-06-14 02:56:17 +0300

    Update game_data.py
    Добавлены новые уровни

--- game_data.py
+++ game_data.py
@@ -1,4 +1,4 @@
-level1 = {
+level0 = {
     'terrain': './levels/0/0_terrain.csv',
     'coins': './levels/0/0_coins.csv',
     'trees': './levels/0/0_trees.csv',
@@ -8,4 +8,45 @@ level1 = {
     'enemies': './levels/0/0_enemies.csv',
     'constraints': './levels/0/0_constraints.csv',
     'player': './levels/0/0_player.csv',
-    'grass': './levels/0/0_grass.csv'}
+    'grass': './levels/0/0_grass.csv',
+    'node_pos': (110, 400),
+    'unlock': 1,
+    'node_graphics': './graphics/overworld/0'
+}
+
+level1 = {
+    'terrain': './levels/1/1_terrain.csv',
+    'coins': './levels/1/1_coins.csv',
+    'trees': './levels/1/1_trees.csv',
+    'fence': './levels/1/1_fence.csv',
+    'building0': './levels/1/1_building0.csv',
+    'building1': './levels/1/1_building1.csv',
+    'enemies': './levels/1/1_enemies.csv',
+    'constraints': './levels/1/1_constraints.csv',
+    'player': './levels/1/1_player.csv',
+    'grass': './levels/1/1_grass.csv',
+    'node_pos': (480, 610),
+    'unlock': 2,
+    'node_graphics': './graphics/overworld/1'
+}
+
+level2 = {
+    'terrain': './levels/2/2_terrain.csv',
+    'coins': './levels/2/2_coins.csv',
+    'trees': './levels/2/2_trees.csv',
+    'fence': './levels/2/2_fence.csv',
+    'building0': './levels/2/2_building0.csv',
+    'building1': './levels/2/2_building1.csv',
+    'enemies': './levels/2/2_enemies.csv',
+    'constraints': './levels/2/2_constraints.csv',
+    'player': './levels/2/2_player.csv',
+    'grass': './levels/2/2_grass.csv',
+    'node_pos': (1050, 400),
+    'unlock': 3,
+    'node_graphics': './graphics/overworld/2'
+}
+
+levels = {0: level0,
+          1: level1,
+          2: level2,
+          }
