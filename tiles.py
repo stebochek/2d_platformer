0687ea8a150ea112cd16f6eab284366cac808830
nelyuboov <114228207+nelyuboov@users.noreply.github.com>
2023-06-15 03:56:51 +0300

    Update tiles.py (class Goal)
    Класс Goal теперь наследует класс AnimatedTile

--- tiles.py
+++ tiles.py
@@ -19,12 +19,13 @@ class StaticTile(Tile):
 
 
 class AnimatedTile(Tile):
-    def __init__(self, size, x, y, path):
+    def __init__(self, size, x, y, path, animation_speed):
         super().__init__(size, x, y)
         self.frames = import_folder(path)
         self.frame_index = 0
         self.image = self.frames[self.frame_index]
-        self.animation_speed = 0.15
+        self.animation_speed = animation_speed
+        self.path = path
 
 
     def animate(self):
@@ -32,6 +33,8 @@ class AnimatedTile(Tile):
         if int(self.frame_index) >= len(self.frames):
             self.frame_index = 0
         self.image = self.frames[int(self.frame_index)]
+        if './graphics/character/end_of_level' in self.path:
+            self.image = pygame.transform.flip(self.image, True, False)
 
     def update(self, x_shift):
         self.animate()
@@ -39,29 +42,17 @@ class AnimatedTile(Tile):
 
 
 class Coin(AnimatedTile):
-    def __init__(self, size, x, y, path):
-        super().__init__(size, x, y, path)
+    def __init__(self, size, x, y, path, value, animation_speed):
+        super().__init__(size, x, y, path, animation_speed)
         center_x = x + int(size / 2)
         center_y = y + int(size / 2)
         self.rect = self.image.get_rect(center=(center_x, center_y))
-        self.animation_speed = 0.15
+        self.value = value
+        self.animation_speed = animation_speed
 
 
-class Goal(Tile):
-    def __init__(self, size, x, y, path):
-        super().__init__(size, x, y)
-        self.frames = import_folder(path)
-        self.frame_index = 0
-        self.image = self.frames[self.frame_index]
-        self.animation_speed = 0.07
+class Goal(AnimatedTile):
+    def __init__(self, size, x, y, path, animation_speed):
+        super().__init__(size, x, y, path, animation_speed)
+        self.animation_speed = animation_speed
 
-    def animate(self):
-        self.frame_index += self.animation_speed
-        if int(self.frame_index) >= len(self.frames):
-            self.frame_index = 0
-        self.image = self.frames[int(self.frame_index)]
-        self.image = pygame.transform.flip(self.image, True, False)
-
-    def update(self, x_shift):
-        self.animate()
-        self.rect.x += x_shift
