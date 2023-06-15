5a396a4a110029488d17908b867f5a5685a86153
nelyuboov <114228207+nelyuboov@users.noreply.github.com>
2023-06-15 04:04:53 +0300

    Update particles.py for get damage for enemies
    Добавлено условие на смерть энеми для воспроизведения анимации смерть

--- particles.py
+++ particles.py
@@ -6,11 +6,15 @@ class ParticleEffect(pygame.sprite.Sprite):
     def __init__(self, pos, type):
         super().__init__()
         self.frame_index = 0
-        self.animation_speed = 0.5
         if type == 'jump':
             self.frames = import_folder('./graphics/character/dust_particles/jump')
+            self.animation_speed = 0.5
         if type == 'land':
             self.frames = import_folder('./graphics/character/dust_particles/land')
+            self.animation_speed = 0.45
+        if type == 'death':
+            self.frames = import_folder('./graphics/enemies/death')
+            self.animation_speed = 0.1
         self.image = self.frames[self.frame_index]
         self.rect = self.image.get_rect(center=pos)
 
